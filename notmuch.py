78cad045176836a9dbc0b15188af87254211d4b6
nogard <nogard@ucsc.edu>
2022-03-30 22:51:14 -0700

    added count up to 10

--- notmuch.py
+++ notmuch.py
@@ -4,5 +4,8 @@ import string
 for i, c in enumerate(string.ascii_lowercase):
     print(f"{i}. lower={c} upper={c.upper()}")
 
+for i in range(10):
+	print(i, end='...')
+print()
 
 print('All Done!! 🐮🔔')
