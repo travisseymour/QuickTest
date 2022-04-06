06f843f11536d1e4bc78e167ef871ff14959821c
nogard <nogard@ucsc.edu>
2022-04-06 10:41:34 -0700

    fixed formatting

--- notmuch.py
+++ notmuch.py
@@ -5,7 +5,7 @@ for i, c in enumerate(string.ascii_lowercase):
     print(f"{i}. lower={c} upper={c.upper()}")
 
 for i in range(10):
-	print(i, end='...')
+    print(i, end='...')
 print()
 
 print('All Done!! 🐮🔔')
